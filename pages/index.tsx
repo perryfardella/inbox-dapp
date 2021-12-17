27615806a43cecb6b378c267e816dd6b171e6302
Perry Fardella <perry.fardella@live.com>
2021-12-17 22:47:34 +0800

    Install ethers.js add a react hook for the provider

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,24 +2,36 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { ethers } from "ethers";
+import { useEffect, useState } from 'react';
 
 // Extend the global window interface to avoid a typescript error here
 // where ethereum can't be found in the browser 
 declare global {
   interface Window {
       ethereum:any;
+      web3:any;
   }
 }
 
 const Home: NextPage = () => {
   // If MetaMask is installed, ethereum is available as a property
   // of the window object
-  if (typeof window.ethereum !== 'undefined') {
-    console.log('MetaMask is installed!');
-  };
+  // if (typeof window.ethereum !== 'undefined') {
+  //   console.log('MetaMask is installed!');
+  // };
+
+  const [provider, setProvider] = useState({})
+
+  useEffect(() => {
+    if (typeof window.ethereum !== 'undefined' || (typeof window.web3 !== 'undefined')) {
+       setProvider(new ethers.providers.Web3Provider(window.ethereum))
+    }
+  }, []);
 
   const connectWallet = () => {
-    window.ethereum.request({ method: 'eth_requestAccounts' });
+    // window.ethereum.request({ method: 'eth_requestAccounts' });
+    console.log(provider);
   }
 
   return (
